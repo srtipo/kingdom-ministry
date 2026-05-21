0fe719e579161b95e70e9f953a4cb88738a96711
victor acosta <42049455+srtipo@users.noreply.github.com>
2026-05-20 20:54:30 -0400

    resolve comments

--- ui/icons/input-icon.tsx
+++
@@ -1,9 +0,0 @@
-import type { ComponentProps } from "react";
-import React from "react";
-import { TextInput as TI } from "react-native-paper";
-
-type Props = ComponentProps<typeof TI.Icon>;
-
-export default function InputIcona(props: Props) {
-  return <TI.Icon {...props} />;
-}
